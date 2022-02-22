f3c2aa6784fe51ff6c64d824319f70899a41ff5e
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-02-21 22:22:19 -0600

    se implementaron variables de entorno con la libreria os.environ

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,10 @@ WORKDIR /app
 
 ADD . .
 
-EXPOSE 8000
+# RUN mkdir static && python manage.py collectstatic
 
-CMD ["gunicorn","--bind",":8000","--workers","3","sistema_clinico_medico.wsgi:application"]
+# EXPOSE 8000
+
+# CMD ["gunicorn","--bind",":8000","--workers","3","sistema_clinico_medico.wsgi:application"]
+
+CMD gunicorn sistema_clinico_medico.wsgi:application --bind 0.0.0.0:$PORT
--- sistema_clinico_medico/settings/settings.py
+++ sistema_clinico_medico/settings/settings.py
@@ -1,18 +1,13 @@
 from pathlib import Path
 import os
-import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-env=environ.Env()
-
-environ.Env.read_env()
-
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
