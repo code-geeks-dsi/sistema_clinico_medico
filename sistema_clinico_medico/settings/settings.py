0b443ac6f8d73a2653ca78592b5ed36ecc646f9a
Ronaldo Martinez <irmv_0311@hotmail.com>
2022-04-12 00:26:00 -0600

    AWS S3 configuracion

--- sistema_clinico_medico/settings/settings.py
+++ sistema_clinico_medico/settings/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -102,12 +103,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
