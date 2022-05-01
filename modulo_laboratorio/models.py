e4757778e112d9adec4fc246d7c075ee7899ba01
Ronald-Ayala <73920299+Ronald-Ayala@users.noreply.github.com>
2022-04-30 22:25:09 -0600

    Update models.py

--- modulo_laboratorio/models.py
+++ modulo_laboratorio/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Parametro(AbstractBaseUser, PermissionsMixin):
-nombre = models.CharField(db_column='UNIDAD',max_length=40,unique=True)
-unidad = models.CharField(db_column='UNIDAD', max_length=40, null=True)
+nombre_parametro = models.CharField(db_column='UNIDAD',max_length=40,unique=True)
+unidad_parametro = models.CharField(db_column='UNIDAD', max_length=40, null=True)
+
