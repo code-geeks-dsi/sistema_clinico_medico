77cdc44543fcd4259e144f733568b5f43083cbdd
Ronald-Ayala <73920299+Ronald-Ayala@users.noreply.github.com>
2022-04-30 23:57:13 -0600

    c

--- modulo_laboratorio/models.py
+++ modulo_laboratorio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Parametro(models.model):
-id_parametro = models.AutoField(primary_key=True)
-nombre_parametro = models.CharField(,max_length=40,null=false, blank=false)
-unidad_parametro = models.CharField(, max_length=40, null=True,blank=false)
+    id_parametro = models.AutoField(primary_key=True)
+    nombre_parametro = models.CharField(,max_length=40,null=false, blank=false)
+    unidad_parametro = models.CharField(, max_length=40, null=True,blank=false)
 
