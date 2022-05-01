35efed7ef7de13968e29737b996f091224bd4709
Carolinna99 <ml17017@ues.edu.sv>
2022-05-01 01:17:06 -0600

    Modelos de paciente, consulta, contieneConsulta, constanciaMedica,Brindaconsulta

--- modulo_expediente/models.py
+++ modulo_expediente/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from pyexpat import model
+from turtle import mode
 from django.db import models
 
 # Create your models here.
@@ -8,7 +10,6 @@ class Paciente(models.Model):
         (2, 'Femenino'),
     )
     id_paciente=models.AutoField(primary_key=True,unique=True)
-    id_expediente=models.ForeignKey(Expediente, models.DO_NOTHING, blank=False, null=False)
     nombre_paciente = models.CharField( max_length=40,blank=False, null=False)
     apellido_paciente = models.CharField( max_length=40,blank=False, null=False)
     fecha_nacimiento_paciente = models.DateField( blank=False,null=False)
@@ -17,26 +18,51 @@ class Paciente(models.Model):
     email_paciente = models.EmailField( max_length=100, blank=False, null=False, unique=True)
     responsable=models.CharField(max_length=40,blank=False,null=False)
 
-class Paciente(models.Model):
-    id_paciente= models.AutoField(primary_key=True)
 
 class Expediente (models.Model):
-    id_expediente= models.AutoField(primary_key=True)
+    id_expediente= models.AutoField(primary_key=True, max_length=8,null=False, blank=False)
+  
 
 class Consulta(models.Model):
     id_consulta= models.AutoField(primary_key=True)
+    constancia_medica= models.OneToOneField(ConstanciaMedica,on_delete=models.DO_NOTHING,parent_link=True,null=False, blank=False)
+    signos_vitales= models.OneToOneField(SignosVitales,on_delete=models.DO_NOTHING,parent_link=True,null=False, blank=False)
+    examen_de_laboratorio= models.OneToOneField(OrdenExamenLaboratorio, on_delete=models.DO_NOTHING, blank=False, null=False)
+    diagnostico=models.CharField(max_length=200, blank=False, null=False)
+    sintoma=models.CharField(max_length=200, blank=False, null=False)
 
-class contiene_consulta(models.Model):
-    id_expediente = models.ForeignKey(Expediente, models.DO_NOTHING, blank=False, null=True)
-    id_consulta = models.ForeignKey(Consulta, models.DO_NOTHING, blank=False, null=True)
+class ContieneConsulta(models.Model):
+    OPCIONES_ESTADO_DE_PAGO=(
+        (1,'No pagado'),
+        (2,'Parcialmente pagado'),
+        (3,'Pagado'),
+    )
+    OPCIONES_FASE=(
+        (1,'Agendado'),
+        (2,'Agregar a cola'),
+        (3,'Anotado'),
+        (4,'Preparado'),
+        (5,'En espera'),
+        (6,'En consulta'),
+        (7,'Atender paciente'),
+        (8,'Ver expediente'),
+        (9,'Finalizar consulta'),
+    )
+    expediente = models.ManyToManyField(Expediente, models.DO_NOTHING, blank=False, null=True)
+    consulta = models.ManyToManyField(Consulta, models.DO_NOTHING, blank=False, null=True)
+    numero_cola=models.IntegerField(max_length=6, blank=False, null=False)
+    fecha_de_cola=models.DateField(default=datetime.now, blank=False, null=False)
+    consumo_medico=models.DecimalField(max_digits=6,decimal_places=2,null=False, blank=False)
+    estado_cola_medica=models.CharField(max_length=20,choices=OPCIONES_ESTADO_DE_PAGO, blank=False,null=False)
+    fase_cola_medica=models.CharField(max_length=20,choices=OPCIONES_FASE, blank=False,null=False)
 
-class Signos_Vitales(models.Model):
+class SignosVitales(models.Model):
     id_signos_vitales= models.AutoField(primary_key=True)
 
-class Orden_Examen_Laboratorio(models.Model):
+class OrdenExamenLaboratorio(models.Model):
     id_orden_examen_laboratorio= models.AutoField(primary_key=True)
 
-class Referencia_Medica(models.Model):
+class ReferenciaMedica(models.Model):
     id_referencia_medica= models.AutoField(primary_key=True)
 
 class Hospital(models.Model):
@@ -51,6 +77,21 @@ class Dosis(models.Model):
 class Medicamento(models.Model):
     id_medicamento= models.AutoField(primary_key=True)
 
-class Constancia_Medica(models.Model):
+class BrindaConsulta(models.Model):
+    OPCIONES_TURNO=(
+        (1,'Matutino'),
+        (2,'Vespertino'),
+    )
+    consulta=models.ForeignKey(Consulta, models.DO_NOTHING, blank=False, null=False)
+    doctor=models.ForeignKey(Doctor, models.DO_NOTHING, blank=False, null=False)
+    consultorio=models.IntegerField(max_length=2, blank=False, null=False)
+    turno=models.CharField(max_length=20,choices=OPCIONES_FASE, blank=False,null=False)
+
+class ConstanciaMedica(models.Model):
     id_constancia_medica= models.AutoField(primary_key=True)
+    consulta= models.ForeignKey(Consulta, models.DO_NOTHING, blank=False, null=False)
+    fecha_de_emision=models.DateField(default=datetime.now, blank=False, null=False)
+    dias_reposo=models.IntegerField(max_length=2, blank=False, null=False)
+    diagnostico_constancia=models.CharField(max_length=200, blank=False, null=False)
+
 
