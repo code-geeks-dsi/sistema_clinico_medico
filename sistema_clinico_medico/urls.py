d0b4a2d62a1f3fab28a27780f3571ced7717ad05
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-05-03 19:22:57 +0000

    probando git dentro del contenedor

--- sistema_clinico_medico/urls.py
+++ sistema_clinico_medico/urls.py
@@ -12,6 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    probando cambios
 """
 from django import views
 from django.contrib import admin
