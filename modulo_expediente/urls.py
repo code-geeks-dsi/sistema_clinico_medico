95d9909547cdd5ec62f6f597a840d1e54909f63c
Carolinna99 <ml17017@ues.edu.sv>
2022-05-04 00:40:46 +0000

    update

--- modulo_expediente/urls.py
+++ modulo_expediente/urls.py
@@ -1,9 +1,10 @@
 from django import views
 from django.contrib import admin
 from django.urls import path
-from modulo_expediente.views import busqueda_paciente
+from modulo_expediente.views import busqueda_paciente , get_paciente
 
 
 urlpatterns = [
     path('paciente/',busqueda_paciente, name='busqueda_paciente'),
+    path('paciente/<int:id_paciente>',get_paciente, name='get_paciente'),
 ]
--- modulo_expediente/views.py
+++ modulo_expediente/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from modulo_expediente.filters import PacienteFilter
 from modulo_expediente.models import Paciente
-
+from django.http import JsonResponse
 # Create your views here.
 
 def busqueda_paciente(request):
@@ -14,3 +14,29 @@ def busqueda_paciente(request):
 def vista_sala_espera(request):
     return render(request,"salaEspera.html")
 
+def get_paciente (request , id_paciente):
+        paciente=list(Paciente.objects.values())
+        lista =[]
+        for i in range(len(paciente)):
+            if paciente[i]["id_paciente"] == id_paciente:
+                diccionario={
+                    "id_paciente":"",
+                    "nombre_paciente":"",
+                    "apellido_paciente":"",
+                    "fecha_nacimiento_paciente":"",
+                    "sexo_paciente":"",
+                    "direccion_paciente":"",
+                    "email_paciente":"",
+                    "responsable":""
+                }
+                diccionario["id_paciente"]= paciente[i]["id_paciente"]
+                diccionario["nombre_paciente"]= paciente[i]["nombre_paciente"]
+                diccionario["apellido_paciente"]= paciente[i]["apellido_paciente"]
+                diccionario["fecha_nacimiento_paciente"]= paciente[i]["fecha_nacimiento_paciente"]
+                diccionario["sexo_paciente"]= paciente[i]["sexo_paciente"]
+                diccionario["direccion_paciente"]= paciente[i]["direccion_paciente"]
+                diccionario["email_paciente"]= paciente[i]["email_paciente"]
+                diccionario["responsable"]= paciente[i]["responsable"]
+                lista.append(diccionario);
+                del diccionario
+        return JsonResponse(lista, safe=False)
