f020535ce7f1419451b3bdafb687a822943239c5
Ronaldo Martinez <irmv_0311@hotmail.com>
2022-05-06 07:11:47 +0000

    Vista Sala Espera V1

--- modulo_expediente/admin.py
+++ modulo_expediente/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from modulo_expediente.models import Paciente
+from modulo_expediente.models import Paciente, Expediente
 # Register your models here.
 admin.site.register(Paciente)
+admin.site.register(Expediente)
