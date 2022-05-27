600f5bea71767a5509cf5486b4851c0e4f64ca14
Ronaldo Martinez <irmv_0311@hotmail.com>
2022-05-27 22:32:47 +0000

    Se agregaron To String

--- modulo_laboratorio/admin.py
+++ modulo_laboratorio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from modulo_laboratorio.models import Categoria, CategoriaExamen, ExamenLaboratorio, Parametro, EsperaExamen, ContieneValor, Resultado
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(CategoriaExamen)
+admin.site.register(ExamenLaboratorio)
+admin.site.register(Parametro)
+admin.site.register(EsperaExamen)
+admin.site.register(ContieneValor)
+admin.site.register(Resultado)
