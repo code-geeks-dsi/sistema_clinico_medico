2bb326f06d1279b67812073ae724bd26b5b3b7e3
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-06-02 00:32:56 +0000

    no se guardan examenes duplicados

--- modulo_laboratorio/views.py
+++ modulo_laboratorio/views.py
@@ -47,13 +47,23 @@ def get_categoria_examen(request, id_categoria):
 def agregar_examen_cola(request):
     id_paciente=request.POST.get('id_paciente',0)
     id_examen_laboratorio=request.POST.get('id_examen_laboratorio',0)
-    examen_item=EsperaExamen.create(id_paciente,id_examen_laboratorio)
-    examen_item.save()
-    response={
-            'type':'success',
-            'title':'Guardado!',
-            'data':'Examen agregado a la cola'
-        }
+    fecha_hoy=datetime.now()
+    examen_item=EsperaExamen.objects.filter(expediente__id_paciente__id_paciente=id_paciente,resultado__examen_laboratorio__id_examen_laboratorio=id_examen_laboratorio,fecha__year=fecha_hoy.year, 
+                        fecha__month=fecha_hoy.month, 
+                        fecha__day=fecha_hoy.day).first()
+    if examen_item is None:
+        examen_item=EsperaExamen.create(id_paciente,id_examen_laboratorio)
+        examen_item.save()
+        response={
+                'type':'success',
+                'title':'Guardado!',
+                'data':'Examen agregado a la cola'
+            }
+    else:
+        response={
+                'type':'warning',
+                'data':'El examen ya existe en la cola!'
+            }
 
     return JsonResponse(response, safe=False)
 
