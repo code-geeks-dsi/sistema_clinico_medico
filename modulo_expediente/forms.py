ff31ce32fdef808d42199aa6078515139b249c1f
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-06-10 19:54:57 -0600

    se elimino tkinter

--- modulo_expediente/forms.py
+++ modulo_expediente/forms.py
@@ -1,7 +1,6 @@
 
 from dataclasses import fields
 from pyexpat import model
-from tkinter.tix import Select
 from django import forms
 from django.forms import ModelForm, NumberInput, TextInput, Select
 from django import forms
