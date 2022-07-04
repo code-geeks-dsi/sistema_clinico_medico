e1bb4760e24fea631deb1d76f38f0f72e7d79a59
Ronaldo-Martinez <irmv_0311@hotmail.com>
2022-07-03 20:06:38 -0600

    Se agrego el calendario

--- static/js/fullCalendar-es.js
+++ static/js/fullCalendar-es.js
@@ -4,7 +4,7 @@ FullCalendar.globalLocales.push(function () {
   var es = {
     code: 'es',
     week: {
-      dow: 1, // Monday is the first day of the week.
+      dow: 7, // Monday is the first day of the week.
       doy: 4, // The week that contains Jan 4th is the first week of the year.
     },
     buttonText: {
