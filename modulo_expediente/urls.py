1790a3db5adde68eb97b4e096da4ecd7e00bc431
Ronaldo Martinez <irmv_0311@hotmail.com>
2022-08-03 08:44:10 +0000

    Se agregaron estilos a crear constancia medica y se agrego el link para descargar pdf

--- modulo_expediente/urls.py
+++ modulo_expediente/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
 
     path('buscar/',buscar_expediente,name='buscar_expediente'),
 
-    path('<int:id_consulta>/constancia-medica/pdf',ConstanciaMedicaPDFView.as_view(),name='constancia-medica'),
+    path('consulta/<int:id_consulta>/constancia-medica/pdf',ConstanciaMedicaPDFView.as_view(),name='constancia-medica'),
     path('constancia-medica/',ConstanciaMedicaCreate.as_view(),name='crear-constancia-medica'),
 
     path('constancia-medica/<str:id>',ConstanciaMedicaView.as_view(),name='constancia-medica'),
