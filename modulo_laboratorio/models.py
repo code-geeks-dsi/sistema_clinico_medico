1d41b0e146ac682a3f34f0bcd08511691ceadf91
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-09-09 16:32:22 +0000

    migraciones  modificacion modelo EsperaExamen(orden de examen de lab) y resultado de examen de laboratorio

--- modulo_laboratorio/models.py
+++ modulo_laboratorio/models.py
@@ -16,28 +16,22 @@ class EsperaExamen(models.Model):
         ('1', 'Cancelado'),
         ('3', 'Pendiente')
     )
-    OPCIONES_FASE=(
-        ('1','Recepción de muestra'),
-        ('2','Resultados en Proceso'),
-        ('3','Resultados Listos'),
-        ('4','Resultados Entregado'),
-        ('5','Proceso Finalizado'),
+    OPCIONES_FASE_ORDEN=(
+        ('RC','Recepcion'),
+        ('EP','En Proceso'),
+        ('RE','Resultados Entregado'),
+        ('PF','Proceso Finalizado')
     )
-    resultado=models.ForeignKey('Resultado', on_delete=models.CASCADE)
     expediente=models.ForeignKey('modulo_expediente.Expediente', on_delete=models.CASCADE)
     estado_pago_laboratorio=models.CharField(max_length=15, default=OPCIONES_ESTADO[1][0], choices=OPCIONES_ESTADO, null=False, blank=False)
-    numero_cola_laboratorio=models.IntegerField(null=False,blank=False)
-    consumo_laboratorio=models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False,default=0)
-    fase_examenes_lab=models.CharField(max_length=25,choices=OPCIONES_FASE, blank=False,null=False,default=OPCIONES_FASE[0][0])
-
+    numero_cola_orden=models.IntegerField(null=False,blank=False)
+    fase_examenes_lab=models.CharField(max_length=2,choices=OPCIONES_FASE_ORDEN, blank=False,null=False,default=OPCIONES_FASE_ORDEN[0][0])
     fecha=models.DateTimeField( default=now, blank=True)
 
     @classmethod
     def create(cls, id_paciente,id_examen_laboratorio):
         expediente=Expediente.objects.get(id_paciente=id_paciente)
         examen_laboratorio=ExamenLaboratorio.objects.get(id_examen_laboratorio=id_examen_laboratorio)
-        resultado=Resultado(examen_laboratorio=examen_laboratorio)
-        resultado.save()
         hoy=datetime.now()
         try:
             numero_cola_laboratorio=EsperaExamen.objects.filter(
@@ -47,7 +41,6 @@ class EsperaExamen(models.Model):
             numero_cola_laboratorio=1
         cola_item = cls(
             expediente=expediente,
-            resultado=resultado,
             numero_cola_laboratorio=numero_cola_laboratorio)
             
         return cola_item
@@ -55,27 +48,22 @@ class EsperaExamen(models.Model):
         return str(self.get_fase_examenes_lab_display())+" - "+str(self.expediente.id_paciente.nombre_paciente)
 
 class Resultado(models.Model):
+    OPCIONES_FASE=(
+        ('1','Recepción de muestra'),
+        ('2','Resultados en Proceso'),
+        ('3','Resultados Listos')
+    )
     id_resultado = models.AutoField(primary_key=True)
     lic_laboratorio = models.ForeignKey('modulo_control.LicLaboratorioClinico', on_delete=models.CASCADE,null=True)
     examen_laboratorio= models.ForeignKey('ExamenLaboratorio', on_delete=models.CASCADE,null=False)
-    #resultado=models.CharField(null=False,blank=True,default="",max_length=25)
+    fase_examenes_lab=models.CharField(max_length=25,choices=OPCIONES_FASE, blank=False,null=False,default=OPCIONES_FASE[0][0])
+    numero_cola_resultado=models.IntegerField(null=False,blank=False)
     observaciones=models.TextField(null=False,blank=True,default="")
     fecha_hora_toma_de_muestra=models.DateTimeField(null=True,blank=True)
     fecha_hora_elaboracion_de_reporte=models.DateTimeField(null=True,blank=True)
 
     def __str__(self):
         return self.examen_laboratorio.nombre_examen
-
-# Clases correspondientes a la receta de examenes de laboratorio
-class OrdenExamenLaboratorio(models.Model):
-    id_orden_examen_laboratorio= models.AutoField(primary_key=True)
-    fecha_programada=models.DateField(default=datetime.now,null=False, blank=False)
-    expediente=models.ForeignKey('modulo_expediente.Expediente',on_delete=models.CASCADE,null=False, blank=False)
-# este item podria incluir costo del examen y descuento para el modulo servicios
-class OrdenExamenLaboratorioItem(models.Model):
-    id_orden_examen_laboratorio_item=models.AutoField(primary_key=True)
-    orden_examen_laboratorio=models.ForeignKey('OrdenExamenLaboratorio',on_delete=models.CASCADE)
-    resultado=models.ForeignKey('Resultado',on_delete=models.CASCADE)
 
 class ContieneValor(models.Model):
     resultado = models.ForeignKey('Resultado', on_delete=models.CASCADE)
@@ -151,11 +139,5 @@ class RangoDeReferencia(models.Model):
     unidad=models.CharField(null=False,blank=True,default="",max_length=8)
     def __str__(self):
         return self.parametro.nombre_parametro+" "+self.descripcion
-
-class ServicioDeLaboratorioClinico(models.Model):
-    id_servicio =models.AutoField(primary_key=True)
-    precio_servicio_clinica=models.DecimalField(max_digits=10,decimal_places=2,null=False, blank=False)
-    examen_de_laboratorio=models.ForeignKey('ExamenLaboratorio',on_delete=models.CASCADE,blank=False,null=False)
-
 
 
