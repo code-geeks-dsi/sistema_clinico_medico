dda768840e91ab7a687cc912e7855e45fa194964
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-10 15:52:18 +0000

    se elimino la herencia de servicio a servicio médico y servicio laboratorio clínico. Además, se agregaron los modelos de publicidad al admin

--- modulo_publicidad/urls.py
+++ modulo_publicidad/urls.py
@@ -4,6 +4,8 @@ from modulo_publicidad.views.AdministracionPublicidad import *
 urlpatterns = [
     path('inicio/', InicioPublicidad.as_view(), name='inicio_publicidad'),
     path('clinica/', ClinicaPublicidad.as_view(), name='clinica_publicidad'),
+    
+    # Crea y Edita promociones
     path('clinica/<int:id_promocion>', PromocionClinica.as_view(), name='clinica_publicidad_promocion'),
     path('laboratorio/', InicioPublicidad.as_view(), name='laboratorio_publicidad'),
     path('farmacia/', InicioPublicidad.as_view(), name='farmacia_publicidad'),
