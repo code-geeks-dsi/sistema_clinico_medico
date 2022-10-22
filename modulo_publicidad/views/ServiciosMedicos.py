b934ffea573915ba07cd9ed5908781de9001f84e
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-22 05:50:21 +0000

    Editar servicio médico

--- modulo_publicidad/views/ServiciosMedicos.py
+++ modulo_publicidad/views/ServiciosMedicos.py
@@ -64,29 +64,26 @@ class editarServicioMedico(View):
     template_name="servicios/medicos/crear_editar.html"
     def get(self, request, *args, **kwargs):
         id_servicio=kwargs.get('id_servicio', None)
-        if id_servicio:
-            servicio=Servicio.objects.get(id_servicio=id_servicio)
-
-            data={
-                'formServicioMedico':ServicioMedicoForm(instance=servicio),
-                'formImagen':ServicioImagenForm()
-            }
-            return render(request, self.template_name, data)
+        servicio=Servicio.objects.get(id_servicio=id_servicio)
+        imagen=ImagenServicio.objects.get(servicio=servicio)
+        servicioMedico=ServicioMedico.objects.get(servicio=servicio)
+        initial_data={
+            "area":servicioMedico.tipo_consulta
+        }
+        servicioMedicoForm=ServicioMedicoForm(instance=servicio, initial=initial_data)
+        data={
+            'formServicioMedico':servicioMedicoForm,
+            'formImagen':ServicioImagenForm(instance=imagen)
+        }
+        return render(request, self.template_name, data)
     def post(self, request, *args, **kwargs):
-        '''
-        Pasos:
-        1. Guardar tipo consulta si existe(Checkbox crear_tipo_consulta). LISTO
-        2. Guardar Servicio.LISTO
-        3. Guardar Servicio Médico y asignarle servicio.LISTO
-        4. Guardar publicación y asignarle el servicio médico.
-        5. Guardar Imagen y asignarle la publicación.
-        '''
+        id_servicio=kwargs.get('id_servicio', None)
+        servicio=Servicio.objects.get(id_servicio=id_servicio)
+        imagen=ImagenServicio.objects.get(servicio=servicio)
         # Recuperando datos
-        formServicioMedico=ServicioMedicoForm(request.POST)
-        formPublicacion=PublicacionForm(request.POST)
-        formImagen=ServicioImagenForm(request.POST)
+        formServicioMedico=ServicioMedicoForm(request.POST,instance=servicio)
+        formImagen=ServicioImagenForm(request.POST,instance=imagen)
         if formServicioMedico.is_valid():
-            if formPublicacion.is_valid():
                 if formImagen.is_valid():
                     #PASO 1
                     crear_tipo_consulta=formServicioMedico.cleaned_data['crear_tipo_consulta']  
@@ -99,23 +96,15 @@ class editarServicioMedico(View):
                     #PASO 2
                     servicio=formServicioMedico.save()
                     #PASO 3
-                    servicioMedico=ServicioMedico(servicio=servicio,tipo_consulta=tipoConsulta)
+                    servicioMedico=ServicioMedico.objects.get(servicio=servicio)
+                    servicioMedico.tipo_consulta=tipoConsulta
                     servicioMedico.save()
                     #PASO 4
-                    publicacion=formPublicacion.save(commit=False)
-                    publicacion.servicio=servicio
-                    publicacion.save()
-                    #PASO 5
-                    imagen=formImagen.save(commit=False)
-                    imagen.publicacion=publicacion
-                    imagen.save()
-
-
-
-
-        data={
-            'formServicioMedico':formServicioMedico,
-            'formPublicacion':formPublicacion,
-            'formImagen':formImagen
-        }
-        return render(request, self.template_name, data)
+                    formImagen.save()
+                    return redirect('editar_servicio_medico', servicio.id_servicio)
+        else:
+            data={
+                'formServicioMedico':formServicioMedico,
+                'formImagen':formImagen
+            }
+            return render(request, self.template_name, data)
