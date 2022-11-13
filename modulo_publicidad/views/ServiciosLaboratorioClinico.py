023dafe286b0c9217875d8e117de9d38634278d0
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-11-13 07:02:34 +0000

    Reparación de error editar servicio laboratorio clínico

--- modulo_publicidad/views/ServiciosLaboratorioClinico.py
+++ modulo_publicidad/views/ServiciosLaboratorioClinico.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from django.views import View
 from django.shortcuts import (render,redirect)
 from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 
 #Propias
 from modulo_expediente.models import TipoConsulta
@@ -81,58 +82,71 @@ class EditarServicioLaboratorio(View):
     def get(self, request, *args, **kwargs):
         id_servicio=kwargs.get('id_servicio', None)
         mensajes=request.session.get('mensajes', [])
-        request.session['mensajes']=[]
-        servicio=Servicio.objects.get(id_servicio=id_servicio)
-        imagen=ImagenServicio.objects.get(servicio=servicio)
-        # try:
+        if request.session.get('mensajes') is not None:
+            del request.session['mensajes']
+        servicio=get_object_or_404(Servicio, id_servicio=id_servicio)
+        try:
+            imagen=ImagenServicio.objects.get(servicio=servicio)
+            formImagen=ServicioImagenForm(instance=imagen)
+        except ImagenServicio.DoesNotExist:
+            formImagen=ServicioImagenForm()
+
         servicioLaboratorio=ServicioLaboratorioClinico.objects.get(servicio=servicio)
         initial_data={
             "examen_laboratorio":servicioLaboratorio.examen_laboratorio
         }
         servicioLaboratorioForm=ServicioLaboratorioForm(instance=servicio, initial=initial_data)
-        # except ObjectDoesNotExist:
-            # servicioLaboratorioForm=ServicioLaboratorioForm(instance=servicio)
+
         data={
             'formServicioLaboratorio':servicioLaboratorioForm,
-            'formImagen':ServicioImagenForm(instance=imagen),
+            'formImagen':formImagen,
             'mensajes': mensajes
         }
         return render(request, self.template_name, data)
     def post(self, request, *args, **kwargs):
         id_servicio=kwargs.get('id_servicio', None)
-        servicio=Servicio.objects.get(id_servicio=id_servicio)
-        imagen=ImagenServicio.objects.get(servicio=servicio)
+        servicio=get_object_or_404(Servicio, id_servicio=id_servicio)
+        try:
+            imagen=ImagenServicio.objects.get(servicio=servicio)
+            formImagen=ServicioImagenForm(request.POST, request.FILES, instance=imagen)
+        except ImagenServicio.DoesNotExist:
+            formImagen=ServicioImagenForm(request.POST, request.FILES)
         # Recuperando datos
         formServicioLaboratorio=ServicioLaboratorioForm(request.POST,instance=servicio)
-        formImagen=ServicioImagenForm(request.POST, request.FILES, instance=imagen)
         if formServicioLaboratorio.is_valid():
                 if formImagen.is_valid():
-                    #PASO 1
-                    crear_tipo_consulta=formServicioLaboratorio.cleaned_data['crear_tipo_consulta']  
-                    if crear_tipo_consulta==True:
-                        nombreTipoConsulta=formServicioLaboratorio.cleaned_data['otro']
-                        tipoConsulta=TipoConsulta(nombre=nombreTipoConsulta)
-                        tipoConsulta.save()
-                    else:
-                        tipoConsulta=formServicioLaboratorio.cleaned_data['area'] 
                     #PASO 2
                     servicio=formServicioLaboratorio.save()
                     #PASO 3
-                    servicioLaboratorio=ServicioLaboratorioClinico.objects.get(servicio=servicio)
-                    servicioLaboratorio.tipo_consulta=tipoConsulta
-                    servicioLaboratorio.save()
-                    #PASO 4
-                    formImagen.save()
-                    request.session['mensajes']=[{
-                        'type':'success',
-                        'data':'Servicio de Laboratorio Clínico '+servicio.nombre+' Modificado'
-                        }] 
-                    return redirect(
-                        'editar_servicio_laboratorio', 
-                        servicio.id_servicio )
-        else:
-            data={
-                'formServicioLaboratorio':formServicioLaboratorio,
-                'formImagen':formImagen
-            }
-            return render(request, self.template_name, data)
+                    try:
+                        examen_laboratorio=formServicioLaboratorio.cleaned_data['examen_laboratorio']   
+                        servicioLaboratorio=ServicioLaboratorioClinico.objects.get(servicio=servicio)
+                        servicioLaboratorio.examen_laboratorio=examen_laboratorio
+                        servicioLaboratorio.save()
+                        #PASO 4
+                        imagen=formImagen.save(commit=False)
+                        imagen.servicio=servicio
+                        imagen.save()
+                        request.session['mensajes']=[{
+                            'type':'success',
+                            'data':'Servicio de Laboratorio Clínico '+servicio.nombre+' Modificado'
+                            }] 
+                        return redirect(
+                            'editar_servicio_laboratorio', 
+                            servicio.id_servicio )
+                    except IntegrityError:
+                        request.session['mensajes']=[{
+                        'type':'warning',
+                        'data':'Servicio de Laboratorio Clínico '+servicio.nombre+' ya existe.'
+                        }]
+                        # servicio.delete()
+        mensajes=request.session.get('mensajes', [])
+        if request.session.get('mensajes') is not None:
+            del request.session['mensajes']
+        data={
+            'formServicioLaboratorio':formServicioLaboratorio,
+            'formImagen':formImagen,
+            'mensajes': mensajes
+        }
+        return render(request, self.template_name, data)
+           
